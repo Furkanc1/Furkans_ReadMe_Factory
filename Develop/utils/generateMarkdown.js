6ac213995beb1acfb6732f61fc865063381292ed
Furkan Coskun <furkdurk7@aol.com>
2023-10-30 16:48:24 -0400

    debugged ReadMe

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,74 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(licenseName) {
+  let extraStuffAtTheEnd = ``;
+  if (licenseName == `BSD`) extraStuffAtTheEnd = `-3-Clause`;
+  else if (licenseName == `GPL`) extraStuffAtTheEnd = `-3.0`;
+  else if (licenseName = `Apache`) extraStuffAtTheEnd = `-2.0`;
+  let licenseLink = `https://opensouce.org/licenses/${licenseName}${extraStuffAtTheEnd}`;
+  return licenseLink;
+}
+
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+
+// const { licenses } = require(`../..`);
+const { licenseTexts } = require(`../utils/licenseTexts`);
+
+function renderLicenseBadge(license) {
+  let licenseName = license.split(``)[0];
+  let licenseBadge = `[![License Badge: ${licenseName}](https://img.shields.io/badge/License-${licenseName}-blue.svg)](${renderLicenseLink(licenseName)})`;
+    return licenseBadge;
+};
+
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(licenseName) {
+  // render license section
+  return licenseTexts[licenseName];
+}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(answers) {
+  // destructure variables from object
+  let { title, email, username, description, license } = answers;
 
-`;
-}
+  let licenseName = license.split(``)[0];
+
+  let generatedTemplate = `
+    # ${ title }
+
+    ${ renderLicenseBadge(license) }
+
+    ${ description }
+
+    ${ preview == true ? `[Preview Application](${preview})` : `` }
+
+    ## Table of Contents (Optional)
+
+    [Installation](#installation)
+    [Usage](#usage)
+    [Questions](#questions)
+    [Credits](#credits)
+    [License](#license)
+
+    ## Installation
+    ### Run the command ${installation} to Install
+
+    # Usage 
+    #### Use ${usage} CLI (Command Line Interface) to run this application!
+
+    ## Licensing
+    ${renderLicenseBadge(license)}
+    #### ${renderLicenseSection(licenseName)}
+    #### ${renderLicenseLink(licenseName)}
+
+    ## Questions
+    #### Github Profile: [*${username}*](https://github.com/${username})
+    #### Contact me or ask me questions at [${email}](mailto:${email}).
+    `;
+    return generatedTemplate;
+};
 
 module.exports = generateMarkdown;
--- index.js
+++ index.js
@@ -1,20 +1,91 @@
+// import ListPrompt from "inquirer/lib/prompts/list";
+
 // TODO: Include packages needed for this application
 const inquirer = require(`inquirer`);
 const generateMarkdown = require(`./Develop/utils/generateMarkdown`);
 
+ const licenses = {
+    none: `none`, 
+    MIT: `MIT`,
+    GPL: `GPL`,
+    Apache: `Apache`,  
+    BSD: `BSD`,
+    CDDL: `CDDL`,
+}
+
 //const fututre const here
+
+const rejexEmailValidation = (value) => {
+    let rejexEmailValidationInner = value.match(
+        /^(([^<>()[]\.,;:\s@"]+(.[^<>()[]\.,;:\s@"]+)*)|.(".+"))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/
+       );
+        return rejexEmailValidationInner;
+}
+const vanillaJavaScriptValidation = (value) => {
+    const validDomains = [ 
+        `com`, 
+        `org`, 
+        `gov`, 
+        `edu`
+    ];
+    // .split returns an array
+    // .includes returns true or false (boolean)
+        let emailAT = value.includes(`@`);
+        if (emailAT == true) {
+            let emailWebsite = value.split(`@`)[1];
+            let emailDomain = emailWebsite.split(`.`)[1];
+            let emailHasValidDomain = validDomains.includes(emailDomain);
+            return value != `` && emailHasValidDomain == true ? true : errorString;
+        } else {
+            return `enter valid email`;
+        }
+}
+
 
 
 // TODO: Create an array of questions for user input
 const questions = [
-        {
-            type: 'input',
-            message: 'What is your RepositoryName?',
-            name: `(userRepoName)`,
-            default: `User Repo Name`,
+    {
+        type: 'input',
+        message: 'What is your RepositoryName?',
+        name: `(userRepoName)`,
+        default: `User Repo Name`,
+    },
+    // githubUserName + githubEmail
+    {
+        type: `input`,
+        name: `githubEmail`,
+        message: `What is your GitHub Email Address?`,
+        filter(value) {
+            return value.toLowerCase();
         },
-        {
-            type: 'input',
+            validate(value) {
+                const validDomains = [ 
+                    `com`, 
+                    `org`, 
+                    `gov`, 
+                    `edu`
+                ];
+                 // .split returns an array
+                  // .includes returns true or false (boolean)
+                    let emailAT = value.includes(`@`);
+                    if (emailAT == true) {
+                        let emailWebsite = value.split(`@`)[1];
+                        let emailDomain = emailWebsite.split(`.`)[1];
+                        let emailHasValidDomain = validDomains.includes(emailDomain);
+                        return value != `` && emailHasValidDomain == true ? true : errorString;
+                    } else {
+                        return `enter valid email`;
+                    }
+            },
+    },
+    {
+        type: `input`,
+        name: `UserName`,
+        message: `What is your GitHub User Name?`
+    },
+    {
+        type: 'input',
             message: 'Why did you build this project?',
             name: 'Reasoning...',
             default: `Personal Use`,
@@ -32,40 +103,77 @@ const questions = [
             default: `(insert description Here)`,
         },
         {
+            type: `input`,
+            name: `App_Based question`,
+            message: `Does this app need testing?`,
+            default: ``
+        },
+        {
+            type: `input`,
+            name: `Installation Process`,
+            message: `What are the installation instructions?`,
+            default: ``
+        },
+        {
+            type: `confirm`,
+            name: `preview?`,
+            message: `Do you have a valid link to either an Image, GIF, or vide of the Application?`,
+            default: false
+        },
+        {
+            type: `input`,
+            name: `preview`,
+            message: `Please enter Link of GIF, Image, or Video of Application`,
+            // when" is a method if inquirer
+            when(response) {
+                return response.preview == true;
+            }
+        },
+        {
+            type: `input`,
+            name: `installation`,
+            message: `What are the installation instrutions for your application?`,
+            default: `NPM Install`
+        },
+        {
+            type: `input`,
+            name: `usage`,
+            message: `How do you Test / Run your application`,
+            default: `Using Node.Js`
+        },
+        {
             type: `list`,
             name: `License`,
             message: `What is your repositories License`,
             choices: [
-                `MIT`, 
-                `GPL`, 
-                `Apache`, 
-                `BSD`, 
-                `CDDL`, 
-                `other`
+                licenses.none,
+                licenses.MIT, 
+                licenses.GPL, 
+                licenses.Apache, 
+                licenses.BSD, 
+                licenses.CDDL, 
             ],
             default: 1,
         },
-        {
+    ];
 
-        }
-];
-
-const generalQuestions = () => {
-    inquirer.prompt(questions).then((answers) => {
-        console.log(`answers to questions:`, answers);
-    // user feedback for whatever
-    }).catch((error) => {
-        if (error.isTtyError) {
+    const generalQuestions = () => {
+        inquirer.prompt(questions).then((answers) => {
+            console.log(`answers to questions:`, answers);
+            let generatedMarkdownTemplate = generateMarkdown(answers);
+            // user feedback for whatever
+        }).catch((error) => {
+            if (error.isTtyError) {
                 console.log(`there was an issue rendering in current environment `);
-        } else {
+            } else {
             
-        };
-    });
-}
+            };
+        });
+    }
 
 // TODO: Create a function to write README file
 const writeToFile = (fileName, data) => {
-    
+    FileSystem.writeFile(`GeneratedREADME.md`, generateMarkdown)
 };
 
 // TODO: Create a function to initialize app
